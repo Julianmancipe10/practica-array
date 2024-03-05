49e0b26d12d5aa1c41a8a85c98f05f1c9fa0e850
Julianmancipe10 <manciperuizcenteno@gmail.com>
2024-03-05 16:20:43 -0500

    Se agrega un nuevo array constructor

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let miArray = [];
 let arrayelementos = [1,2,3,4,5];
 let arrayelementosvarios = [1,"dos", true , null ];
-
+let arrayconstructor = new Array ("uno", "dos", "tres","cuatro","cinco" );
