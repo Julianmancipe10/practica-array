aa44ca7a4ea05b0c144edc15376fa1e2d3151cdb
Julianmancipe10 <manciperuizcenteno@gmail.com>
2024-03-05 18:04:16 -0500

    Se crea un array recorriendo la frecuencia numerica con array.from de la variable arrayelementos y se agrgan los textos para expecificar cada array

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
+//Array vacio
 let miArray = [];
+//array con elementos 
 let arrayelementos = [1,2,3,4,5];
+//array con difertentes tipos de datos
 let arrayelementosvarios = [1,"dos", true , null ];
+//array utilizando constructor array()
 let arrayconstructor = new Array ("uno", "dos", "tres","cuatro","cinco" );
+//frecuencia numerica con array.from
+let secuencianumerica = array.from({length: 5}, function(_,index) {
+    return index + 1
+});
+console.log(secuencianumerica);
