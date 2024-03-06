04519ea3429037fbd463fb637bdd6879aac55d36
Julianmancipe10 <manciperuizcenteno@gmail.com>
2024-03-06 08:11:19 -0500

    Se hacen actulizaciones en el codigo

--- index.js
+++ index.js
@@ -1,34 +1,38 @@
 //Array vacio
 let miArray = [];
+
 //array con elementos 
 let arrayelementos = [1,2,3,4,5];
+
 //array con difertentes tipos de datos
 let arrayelementosvarios = [1,"dos", true , null ];
+
 //array utilizando constructor array()
-let arrayconstructor = new Array ("uno", "dos", "tres","cuatro","cinco" );
+let arrayconstructor = Array ("uno", "dos", "tres","cuatro","cinco" );
+
 //frecuencia numerica con array.from
-let secuencianumerica = array.from({length: 5}, function(_,index) {
+let secuencianumerica = Array.from({length: 5}, function(_,index) {
     return index + 1
 });
-console.log(secuencianumerica);
+
 //array utilizando array fill()
 let longitud = 5;
 let valor = 10;
 let arrayrepetido = new Array(longitud).fill(valor);
-console.log(arrayrepetido); 
+ 
 //array con string.split()
 let cadenaTexto = "Hola amigos, me llamo Julian";
 let arrayTexto = cadenaTexto.split(" ");
-console.log(arrayTexto);
+
 //array con string.split(...)
 let array1 =[1,2,3];
 let array2 =[4,5,6];
 let combinacionarray = [...array1,...array2];
-console.log(combinacionarray);
+
 //array utilizando set y spread 
 let arrayduplicados =[50,50,70,80,90,90];
 let arraysinduplicados =[...new Set(arrayduplicados)];
-console.log(arraysinduplicados);
+
 //creaciom de un array con valores dinamicos echos conn una funcion 
 function generarArrayAleatorio() {
     let longitud = Math.floor(Math.random() * 10) + 1;
@@ -39,50 +43,48 @@ function generarArrayAleatorio() {
     return arrayGenerado;
   }
 let arrayGenerado = generarArrayAleatorio();
-console.log(arrayGenerado);
+
 //  longitud de ub array
-let Array = [77, 90, 30, 47, 50];
+let arrayLong = [77, 90, 30, 47, 50];
 let longitudarray = Array.length;
-console.log("La longitud del array es: " + longitud);
+
 //agregar elemento final al array utilizando push vamos a agrgarle esto al array llamdao arrayelementos
 arrayelementos.push(6)
 arrayelementos.push(7)
 arrayelementos.push(8)
-console.log("arrays agregados:", arrayelementos);
+
 // eliminar el ultimo elementoal array utulizando pop
 let arrayeliminacion = ["uno","dos","tres",];
-let ultimoelementoeliminado = arrayeliminacion.pop();
-console.log("Elemento eliminado:", ultimoelementoEliminado);
-console.log("Array después de eliminar el último elemento:", arrayeliminacion);
+let ultimoElementoEliminado = arrayeliminacion.pop();
+
 // agragar elemeno al inicio de el array 
 let nuevoElemento1 = "cero" ;
 let nuevoElemento2 = "negativo";
 arrayeliminacion.unshift(nuevoElemento2);
 arrayeliminacion.unshift(nuevoElemento1);
-console.log("Array después de agregar al principio:", arrayeliminacion);
+
 // eliminar el primer elemento del array arrayeliminacion  utilizando shift
 let primerElementoEliminado = arrayeliminacion.shift();
-console.log("Elemento eliminado al principio:", primerElementoEliminado);
-console.log("Array después de eliminar el primer elemento:", arrayeliminacion);
+
 //Para obtener una opcion del array arrayeliminacion  se utiliza slice()
 let porcionArray = arrayeliminacion.slice(1, 3);
-console.log("Porción del array:", porcionArray);
+
 // Convertir el array en un string usando join()
 let arraystring = ["uno", "dos", "tres"];
 let arrayComoString = arraystring.join(", ");
-// Mostrar el string resultante en la consola
-console.log(arrayComoString);
+
 // Obtener una subsección del array desde el segundo elemento desde el final hasta el final
 let arrayseccion = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let subseccion = array.slice(-3);
-console.log("Subsección del array:", subseccion);
+let subseccion = Array(arrayseccion.slice(-3));
+
+
 //Eliminacion  de una porción del array sin modificar el original usando slice()
 let originasinmodificarlArray = [1, 2, 3, 4, 5];
-let removedItems = originalArray.slice(2, 4); 
-console.log("Elementos eliminados:", removedItems);
-console.log("Original array:",originasinmodificarlArray);
+let removedItems = Array(originasinmodificarlArray.slice(2, 4));
+
 // Agregar múltiples elementos al principio del array utilizando unshift() con spread operator
 let Arraymultiple = [3, 4, 5];
 let newElements = [1, 2];
-originalArray.unshift(...newElements);
-console.log(Arraymultiple);
+Arraymultiple.unshift(...newElements);
+
+
